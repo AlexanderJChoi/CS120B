6cbc14db443b1d98c65a4f202b73dad4662fe976
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2019-04-03 21:40:38 -0700

    lab 2 part 4

---
+++ achoi035_lab02_part4/achoi035_lab02_part4/main.c
@@ -0,0 +1,48 @@
+/*
+ * achoi035_lab02_part4.c
+ *
+ * Created: 4/3/2019 9:12:02 PM
+ * Author : Alexander
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0x00; PORTC = 0xFF;
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	unsigned char tempA = 0x00, tempB = 0x00, tempC = 0x00, tempD = 0x00;
+	unsigned int tempSum = 0x00;
+	
+    while (1) 
+    {
+		tempA = PINA;
+		tempB = PINB;
+		tempC = PINC;
+		tempSum = tempA + tempB + tempC;
+		
+		if(tempSum > 140) {
+			tempD = tempD | 0x01;
+		} else {
+			tempD = tempD & 0xFE;
+		}
+		
+		if(((tempA >= tempC) && (tempA - tempC > 80)) || ((tempC >= tempA) && (tempC - tempA > 80))) {
+			tempD = tempD | 0x02;
+		} else {
+			tempD = tempD & 0xFD;
+		}
+		
+		//We'll display a binary number on D7 ... D2. This number multiplied by 2 will be the approximate weight
+		tempD = (tempD & 0x03) | (((tempSum) << 1) & 0x0FC);
+		
+		
+		PORTD = tempD;
+    }
+}
+
