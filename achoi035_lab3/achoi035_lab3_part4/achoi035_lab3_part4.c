e9389cfae203e26106fbdaf66607b915d269adba
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2019-04-08 19:59:55 -0700

    create lab 3 part 4

---
+++ achoi035_lab3/achoi035_lab3_part4/achoi035_lab3_part4.c
@@ -0,0 +1,20 @@
+/*	Partner 1 Name & E-mail: Alexander Choi, achoi035@ucr.edu
+ *	Partner 2 Name & E-mail:
+ *	Lab Section: 26
+ *	Assignment: Lab 3  Exercise 4
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
