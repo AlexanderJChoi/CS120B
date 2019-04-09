98f9eec70a9e85d2f0c773780e2a4685f7244bc6
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2019-04-08 19:58:20 -0700

    complete lab 3 part 3

--- achoi035_lab3/achoi035_lab3_part3/achoi035_lab3_part3.c
+++ achoi035_lab3/achoi035_lab3_part3/achoi035_lab3_part3.c
@@ -6,15 +6,65 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-
 #include <avr/io.h>
+
+// Bit-access function
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
 
 
 int main(void)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRC = 0xFF; PORTC = 0x00;
+	
+	unsigned char fuelLevel = 0x00, ledOut = 0x00, analogIn = 0x00;
+	/* Replace with your application code */
+	while (1)
+	{
+		fuelLevel = PINA & 0x0F;
+		analogIn = PINA & 0x70;
+		ledOut = 0;
+		ledOut = SetBit(ledOut, 6, 1);
+		
+		switch (fuelLevel)
+		{
+			case 15:
+			case 14:
+			case 13:
+			ledOut = SetBit(ledOut, 0, 1);
+			case 12:
+			case 11:
+			case 10:
+			ledOut = SetBit(ledOut, 1, 1);
+			case 9:
+			case 8:
+			case 7:
+			ledOut = SetBit(ledOut, 2, 1);
+			case 6:
+			case 5:
+			ledOut = SetBit(ledOut, 3, 1);
+			ledOut = SetBit(ledOut, 6, 0);
+			case 4:
+			case 3:
+			ledOut = SetBit(ledOut, 4, 1);
+			case 2:
+			case 1:
+			ledOut = SetBit(ledOut, 5, 1);
+			case 0:
+			break;
+			default:
+			break;
+		}
+		
+		if(GetBit(analogIn, 4) && GetBit(analogIn, 5) && !GetBit(analogIn, 6)) {
+			ledOut = SetBit(ledOut, 7, 1);
+		}
+		
+		PORTC = ledOut;
+	}
 }
-
