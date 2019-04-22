2b42d6a78d36c78bd522dc0fd54612d78bcb709f
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2019-04-22 10:42:28 -0700

    fix lab 5 part 3

--- achoi035_ayin005_lab5/achoi035_ayin005_lab5_part3/achoi035_ayin005_lab5_part3.c
+++ achoi035_ayin005_lab5/achoi035_ayin005_lab5_part3/achoi035_ayin005_lab5_part3.c
@@ -9,40 +9,22 @@
 
 #include <avr/io.h>
 
-enum State {patternA_0, patternA_1, patternA_2, patternB_0, patternB_1, patternB_2} state;
-unsigned short count;
+enum State {patternA_0, patternA_1, patternA_2} state;
 unsigned char lights;
 	
 void tock() {
-	unsigned char button = ~PINA & 0x01;
-	count++;
+	unsigned char buttonAdvanceSequence = ~PINA & 0x01;
 	
 	switch(state) {
 		case patternA_0:
-			lights = 0x01;
 			break;
 		case patternA_1:
+			break;
 		case patternA_2:
-			if(count == 0xFFFF) {
-				if(lights == 0x80) {
-					lights = 0x01;
+			if(lights == 0x80) {
+				lights = 0x01;
 				} else {
-					lights <<= 1;
-				}
-				count = 0;
-			}
-			break;
-		case patternB_0:
-			lights = 0b01010101;
-			break;
-		case patternB_1:
-		case patternB_2:
-			if(count == 0xFFFF) {
 				lights <<= 1;
-				if(!(lights & 0x80)) {
-					lights |= 0x01;
-				} 
-				count = 0;
 			}
 			break;
 		default:
@@ -51,38 +33,21 @@ void tock() {
 	
 	switch(state) {
 		case patternA_0:
-			state = patternA_1;
+			if(buttonAdvanceSequence) {
+				state = patternA_0;
+			} else {
+				state = patternA_1;
+			}
 			break;
 		case patternA_1:
-			if(button) {
-				state = patternA_1;
-			} else {
+			if(buttonAdvanceSequence) {
 				state = patternA_2;
+			} else {
+				state = patternA_1;
 			}
 			break;
 		case patternA_2:
-			if(button) {
-				state = patternB_0;
-			} else {
-				state = patternA_2;
-			}
-			break;
-		case patternB_0:
-			state = patternB_1;
-			break;
-		case patternB_1:
-			if(button) {
-				state = patternB_1;
-			} else {
-				state = patternB_2;
-			}
-			break;
-		case patternB_2:
-			if(button) {
-				state = patternA_0;
-			} else {
-				state = patternB_2;
-			}
+			state = patternA_0;
 			break;
 		default:
 			break;
@@ -98,8 +63,7 @@ int main(void)
 	DDRA = 0x00; PORTA = 0xFF;
 	DDRB = 0xFF; PORTB = 0x00;
 	state = patternA_0;
-	count = 0;
-	lights = 0;
+	lights = 0x01;
 	
     while (1) 
     {
