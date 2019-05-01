8e6015835a6e5cc49f39e8b33ab9816bf84e7b33
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2019-04-30 20:15:58 -0700

    create submission for lab 8

---
+++ achoi035_ayin005_lab8/achoi035_ayin005_lab8/achoi035_ayin005_lab8_part4.c
@@ -0,0 +1,65 @@
+/*	Partner 1 Name & E-mail: Alexander Choi, achoi035@ucr.edu
+ *	Partner 2 Name & E-mail: ALexander Yin, ayin005@ucr.edu
+ *	Lab Section: 26
+ *	Assignment: Lab 8  Exercise 4
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+
+
+void ADC_init() {
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: setting this bit enables analog-to-digital conversion.
+	// ADSC: setting this bit starts the first conversion.
+	// ADATE: setting this bit enables auto-triggering. Since we are
+	//        in Free Running Mode, a new conversion will trigger whenever
+	//        the previous conversion completes.
+}
+
+enum State {read } state;
+unsigned char out_B, out_D;
+
+void tick() {
+	unsigned short x = ADC;
+	x-= 40;
+	x /= 10;
+	x = 8 - x;
+	out_B = 0xFF;
+	
+	
+	switch(state) {
+		case read:
+			state = read;
+			break;
+	}
+	
+	switch(state) {
+		case read:
+			out_B >>= x;
+			break;
+	}
+	
+	PORTB = out_B;
+	PORTD = out_D;
+}
+
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	ADC_init();
+	
+	out_B = 0;
+	out_D = 0;
+	state = read;
+    /* Replace with your application code */
+    while (1) 
+    {
+		tick();
+    }
+}
+
