db61459a75d0a9aaaa44fd87409587adee84904e
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2019-05-15 09:42:31 -0700

    complete lab 11 prelim

---
+++ achoi035_ayin005_lab11/achoi035_ayin005_lab11_prelim/achoi035_ayin005_lab11_prelim.c
@@ -0,0 +1,38 @@
+/*	Partner 1 Name & E-mail: Alexander Choi, achoi035@ucr.edu
+ *	Partner 2 Name & E-mail: ALexander Yin, ayin005@ucr.edu
+ *	Lab Section: 26
+ *	Assignment: Lab 11  prelim
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#include "keypad.h"
+#include "io.c"
+
+
+int main(void)
+{
+    unsigned char x;
+    DDRB = 0xFF; PORTB = 0x00; // PORTB set to output, outputs init 0s
+    DDRC = 0xF0; PORTC = 0x0F; // PC7..4 outputs init 0s, PC3..0 inputs init 1s
+    while(1) {
+	    x = GetKeypadKey();
+	    switch (x) {
+		    case '\0': PORTB = 0x1F; break; // All 5 LEDs on
+		    case '1': PORTB = 0x01; break; // hex equivalent
+		    case '2': PORTB = 0x02; break;
+
+		    // . . . ***** FINISH *****
+
+		    case 'D': PORTB = 0x0D; break;
+		    case '*': PORTB = 0x0E; break;
+		    case '0': PORTB = 0x00; break;
+		    case '#': PORTB = 0x0F; break;
+		    default: PORTB = 0x1B; break; // Should never occur. Middle LED off.
+	    }
+    }
+
+}
+
