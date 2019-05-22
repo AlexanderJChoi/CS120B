164976aa6c3bdb3f84fdc00de07d4885e763033b
Alexander Choi <32470094+AlexanderJChoi@users.noreply.github.com>
2019-05-22 10:21:39 -0700

    create integration bench

---
+++ achoi035_finalproj/achoi035_finalproj_integration/achoi035_finalproj_integration.c
@@ -0,0 +1,21 @@
+/* Name & E-mail: Alexander Choi, achoi035@ucr.edu
+ *	Lab Section: 26
+ *	Assignment: Final Project
+ *	
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+
+#include <avr/io.h>
+#include "nokia5110.h"
+#include "nokia5110.c"
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
